3f23451cc6a5c195b599fe0dbeba370c904bec0f
Tom Wilderspin <tomwilderspin@gmail.com>
2020-10-06 17:33:36 +0100

    setup routing

--- handlers/APIService.js
+++ handlers/APIService.js
@@ -1,6 +1,37 @@
-export const run = async event => Promise.resolve({
-  statusCode: 200,
-  body: JSON.stringify({ hello: 'poop', path: event.path }),
-});
+import { companySearch } from '../lib/Controller/Search';
+
+const RouteControllers = {
+  '/search/companies': { GET: companySearch },
+};
+
+function routeEvent(router = {}) {
+  return event => {
+    const { path = '', httpMethod = '' } = event;
+    if (!path || !httpMethod) {
+      throw new Error('malformed event context');
+    }
+    const { [path]: { [httpMethod.toUpperCase()]: controller = null } = {} } = router;
+    if (controller === null) {
+      return Promise.resolve({ code: 404, data: { message: 'not found' } });
+    }
+    return controller(event);
+  };
+}
+
+export const run = async event => {
+  const router = routeEvent(RouteControllers);
+  try {
+    const { code = 200, data = {} } = await router(event).catch(error => {
+      const { code: errorCode = 500, message = '' } = error;
+      return { code: errorCode, data: { message } };
+    });
+    return { statusCode: code, body: JSON.stringify(data) };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message }),
+    };
+  }
+};
 
 export default run;
--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   output: {
     libraryTarget: 'commonjs2',
     path: path.join(__dirname, '.webpack'),
-    filename: '[name][hash].js',
+    filename: '[name].js',
     sourceMapFilename: '[file].map',
   },
 };
