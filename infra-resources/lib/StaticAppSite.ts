056ed4dde0f992d5f3672ce9c1a97712bf49e631
Tom Wilderspin <tomwilderspin@gmail.com>
2020-12-28 18:32:10 +0000

    setting up app CF distro

--- infra-resources/lib/StaticAppSite.ts
+++ infra-resources/lib/StaticAppSite.ts
@@ -1,11 +1,13 @@
-import { Construct } from "@aws-cdk/core";
+import { Construct, Stack } from "@aws-cdk/core";
 import * as CDK from '@aws-cdk/core';
 import * as S3 from '@aws-cdk/aws-s3';
 import * as IAM from '@aws-cdk/aws-iam';
+import * as CloudFront from '@aws-cdk/aws-cloudfront';
 
 export interface StaticAppSiteProps {
   domainName: string
   appSubDomain: string
+  certArn: string
 }
 
 export default class StaticAppSite extends Construct {
@@ -16,11 +18,6 @@ export default class StaticAppSite extends Construct {
     const siteDomain = `${props.appSubDomain}.${props.domainName}`;
     new CDK.CfnOutput(this, 'SiteUrl', { value: `https://${siteDomain}` });
 
-    // IAM policies.
-    const appBucketAccessPolicy = new IAM.PolicyStatement({
-
-    })
-
     // web app bucket.
     const corsRule = { 
       allowedMethods: [S3.HttpMethods.GET],
@@ -28,7 +25,7 @@ export default class StaticAppSite extends Construct {
       allowedHeaders: ['*'],
     }
     const appBucket = new S3.Bucket(this, 'appBucket', {
-      bucketName: 'web-application',
+      bucketName: 'chg-web-app',
       cors: [corsRule],
       publicReadAccess: false,      
       versioned: true,
@@ -37,6 +34,48 @@ export default class StaticAppSite extends Construct {
       removalPolicy: CDK.RemovalPolicy.RETAIN, 
     });
 
+    // cdn (Cloud Front)
+
+    const appCdn = new CloudFront.Distribution(this, 'ChgWebApp', {
+      //todo add some config here...
+    });
+
+    /**
+     * const distribution = new cloudfront.CloudFrontWebDistribution(this, 'SiteDistribution', {
+            aliasConfiguration: {
+                acmCertRef: certificateArn,
+                names: [ siteDomain ],
+                sslMethod: cloudfront.SSLMethod.SNI,
+                securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_1_2016,
+            },
+            originConfigs: [
+                {
+                    customOriginSource: {
+                        domainName: siteBucket.bucketWebsiteDomainName,
+                        originProtocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+                    },          
+                    behaviors : [ {isDefaultBehavior: true}],
+                }
+            ]
+        });
+     */
+    
+
+    // IAM policies.
+    const appBucketGetObjAccessPolicy = new IAM.PolicyStatement({
+      effect: IAM.Effect.ALLOW,
+      actions: ['s3:GetObject'],
+      resources: [appBucket.arnForObjects("*")],
+    });
+    const appBucketListObjAccessPolicy = new IAM.PolicyStatement({
+      effect: IAM.Effect.ALLOW,
+      actions: ['s3:ListBucket'],
+      resources: [appBucket.bucketArn],
+    });
+
+    //assign bucket policy
+    appBucket.addToResourcePolicy(appBucketGetObjAccessPolicy);
+    appBucket.addToResourcePolicy(appBucketListObjAccessPolicy); 
   }
 
 }
